1966acf8b1282b0a135d06e7ff48b2fad59709fc
Paul Schnipper <schnippa@uni-bremen.de>
2021-02-22 15:45:59 +0100

    nlg_ros use action

--- suturo_nlg/scripts/nlg_ros.py
+++ suturo_nlg/scripts/nlg_ros.py
@@ -5,10 +5,8 @@ import rospy
 # interface with knowledge
 import rosprolog_client
 # Messages
-from std_msgs.msg import String
-from nlg_msgs.msg import MeaningRepresentation, KeyValuePair
-import tmc_msgs.msg
-from tmc_msgs.msg import Voice
+import actionlib
+import nlg_msgs.msg
 # connection to nlg.py3 script
 import zmq
 from tinyrpc import RPCClient, RPCProxy
@@ -23,46 +21,57 @@ rpc_client = RPCClient(
 gen_server = None
 
 
-def callback(data):    
-    list_send = [] # can be converted to JSON for the RPC call
-    for kvp in data.role_values:
-        kvp.value = knowledge_translate(kvp.key, kvp.value)
-        list_send.append((kvp.key, kvp.value))
+class NlgAction:
+    # create messages that are used to publish feedback/result
+    _feedback = nlg_msgs.msg.LanguageGenerationFeedback()
+    _result = nlg_msgs.msg.LanguageGenerationResult()
 
-    value_from_s_nlg = gen_server.generate_text(list_send)
-    if value_from_s_nlg.startswith("!ERROR!~"):
-        error_msg = value_from_s_nlg.split("~")[1]
-        rospy.logerror(error_msg)
-        pub_sentence = "An error accured in the natural language generation pipeline. The error given was: " + error_msg
-    else:
-        pub_sentence = value_from_s_nlg
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, nlg_msgs.msg.LanguageGenerationAction,
+                                                execute_cb=self.execute_cb, auto_start=False)
+        self._as.start()
 
-    textrenderer_pub.publish(pub_sentence)
-    talk_request_pub.publish(Voice(sentence=pub_sentence, language=tmc_msgs.msg.Voice.kEnglish))
-    # taken from /hsrb_battery_notifier/battery_notifier.py I don't know what the k stands for either
+    def execute_cb(self, goal):
+        list_send = []  # can be converted to JSON for the RPC call
+        for kvp in goal.key_value_pairs:
+            kvp.value = knowledge_translate(kvp.key, kvp.value)
+            list_send.append((kvp.key, kvp.value))
+
+        value_from_s_nlg = gen_server.generate_text(list_send)
+        if value_from_s_nlg.startswith("!ERROR!~"):
+            error_msg = value_from_s_nlg.split("~")[1]
+            rospy.logerror(error_msg)
+            self._feedback.generated_sentence = "An error accured in the natural language generation pipeline. The error given was: " + error_msg
+
+        else:
+            self._feedback.generated_sentence = value_from_s_nlg
+
+        self._as.publish_feedback(self._feedback)
+        self._result.error_code = 1
+        self._as.set_succeeded(self._result)
 
 
+@staticmethod
 def knowledge_translate(id_type, thing_id):
-    if (id_type == "object_id" or id_type == "object_id_2"):
+    if id_type == "object_id" or id_type == "object_id_2":
         prolog_query = "object_tts('http://www.semanticweb.org/suturo/ontologies/2020/3/objects#" + thing_id + "', Name)"    
-    elif (id_type == "start_surface_id" or id_type ==  "goal_surface_id"):
-        #TODO this has to be checked once knowledge is done
+    elif id_type == "start_surface_id" or id_type == "goal_surface_id":
+        #  TODO this has to be checked once knowledge is done
         prolog_query = "surface_tts(" + thing_id + "', Name)" 
-    elif (id_type == "start_room_id" or id_type ==  "goal_room_id"):
-        #TODO this has to be checked once knowledge is done
+    elif id_type == "start_room_id" or id_type == "goal_room_id":
+        #  TODO this has to be checked once knowledge is done
         prolog_query = "room_tts(" + thing_id + "', Name)"
     solutions = prolog.all_solutions(prolog_query)
     return solutions[0]['Name']
 
 
 if __name__ == '__main__':
-    talk_request_pub = rospy.Publisher('/talk_request', Voice, queue_size=1)
-    textrenderer_pub = rospy.Publisher('/textrenderer', String, queue_size=1) # for gazebo
-    rospy.Subscriber("nlg_requests", MeaningRepresentation, callback)
     rospy.init_node('natural_language_generator', anonymous=True)
     rospy.loginfo("NLG_ROS: Connecting to nlg.py3")
     gen_server = rpc_client.get_proxy()
     rospy.loginfo(gen_server.test_connection())
     # run forever and wait for incoming msgs
+    server = NlgAction("nlg_requests")
     rospy.spin()
 
