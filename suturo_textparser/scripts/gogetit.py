2cf25223ee6750458372c556aecbe9e7f8dfd98b
Paul Schnipper <schnippa@uni-bremen.de>
2021-06-08 11:47:53 +0200

    go and get it person based parser

--- suturo_textparser/scripts/gogetit.py
+++ suturo_textparser/scripts/gogetit.py
@@ -3,7 +3,7 @@
 import re
 from parsetron import Set, Regex, Optional, OneOrMore, Grammar, RobustParser
 import rospy
-from nlp_msgs.msg import FetchRequest
+from nlp_msgs.msg import GoAndGetIt
 from std_msgs.msg import String
 import rosprolog_client
 
@@ -22,41 +22,28 @@ def get_items():
 
 class TransportGrammar(Grammar):
     article = Set(['a', 'an', 'the', 'any', 'some'])
-    actionTriadic = Set(['bring', 'fetch', 'get', 'give', 'pass'])
-    beneficiary = Set(['me'])
-    room = Set(['outside', 'bar', 'hall', 'attic', 'basement', 'bathroom', 'bedroom', 'cellar', 'dining area', 'dining room', 'garden', 'greenhouse', 'guest bathroom', 'kitchen', 'larder', 'living room', 'lobby', 'pantry', 'winter garden'])
+    personLeft = Set(['person left'])
+    personRight = Set(['person right'])
+    beneficiary = personLeft | personRight
     item = get_items()
-    politeStart = Set(['could you', 'could you please', 'please', 'pretty please', 'would you', 'would you please'])
-    politeEnd = Set(['please', 'pretty please'])
-    frm = Set(['from'])
     to = Set(['to'])
-    it = Set(['it'])
-    in_ = Set(['in'])
-    there_is = Set(['there is'])
     color = Set(['black','blue','brown','cyan','green','magenta','orange','pink','purple','red','teal','yellow','white'])
     itemCol = Optional(color) + item
-    roomArt = Optional(article) + room
     itemArt = Optional(article) + itemCol #  | it
-    source = frm + roomArt
     destination = to + beneficiary
-    transport2Beneficiary1 = actionTriadic + beneficiary + itemArt + Optional(source)
-    transport2Beneficiary2 = there_is + itemArt + in_ + roomArt + actionTriadic + it + destination
-    command = transport2Beneficiary1 | transport2Beneficiary2
-    commandPol = Optional(politeStart) + command + Optional(politeEnd)
-    GOAL = OneOrMore(commandPol)
+    command = itemArt + Optional(destination)
+    GOAL = OneOrMore(command)
 
 
-def recursive_search_room(node):
-    if node.is_leaf() and node.__str__().startswith("(room"):
-        regresult = re.findall(r"\"([A-Za-z0-9_]+)\"\)", node.__str__())
-        return " ".join(regresult)
-    elif node.is_leaf():
-        return ""
+def search_benificiary(node):
+    string = node.__str__()
+    if "personLeft" in string:
+        return "left"
+    elif "personRight" in string:
+        return "right"
     else:
-        a = []
-        for c in node.children:          
-            a.append(recursive_search_room(c))
-        return "".join(a)
+        return "both"
+    
 
 
 def recursive_search_object(node):
@@ -73,13 +60,18 @@ def recursive_search_object(node):
 
 
 def callback(data):
-    # tg = TransportGrammar()
-    # tg.set_items()
     parser = RobustParser(TransportGrammar())
     tree, result = parser.parse(data.data)
-    msg = FetchRequest()
+    msg = GoAndGetIt()
     if tree:
-        msg.perceived_room_name = recursive_search_room(tree)
+        benificiary = search_benificiary(tree)
+        if benificiary == "left":
+            msg.person_left = True
+        elif benificiary == "right":
+            msg.person_right = True
+        else:
+            msg.person_left = True
+            msg.person_right = True
         msg.perceived_object_name = recursive_search_object(tree)
     
     pub.publish(msg)
@@ -89,9 +81,9 @@ if __name__ == '__main__':
     try:
         # Start the publisher node
         # rospy.init_node('suturo_GoAndGetIt_Parser', anonymous=True)
-        pub = rospy.Publisher('sp_output', FetchRequest, queue_size=10)
+        pub = rospy.Publisher('sp_output', GoAndGetIt, queue_size=10)
         # Start the subscriber node
-        rospy.Subscriber('go_and_get_it_sentence', String, callback)
+        rospy.Subscriber('sp_input', String, callback)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
