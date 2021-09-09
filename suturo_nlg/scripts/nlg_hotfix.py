f7cadc428e0b8b34af3415e21e8a45562d70b151
Paul Schnipper <schnippa@uni-bremen.de>
2021-09-09 16:07:08 +0200

    hotfix puplish voice

--- suturo_nlg/scripts/nlg_hotfix.py
+++ suturo_nlg/scripts/nlg_hotfix.py
@@ -16,13 +16,13 @@ def callback(data):
     msg = TalkRequestActionGoal()
     voice = Voice()
     voice.sentence = data.feedback.feedback.data
-    msg.goal.data = voice
+    msg = voice
     pub.publish(msg)
 
 
 if __name__ == '__main__':
     rospy.init_node('nlg_hotfix', anonymous=True)
-    pub = rospy.Publisher('/talk_request_action/goal', TalkRequestActionGoal, queue_size=10)
+    pub = rospy.Publisher('/talk_request', Voice, queue_size=10)
     rospy.Subscriber("/nlg_requests/feedback", nlg_msgs.msg.LanguageGenerationActionFeedback, callback)
     rospy.spin()
 
