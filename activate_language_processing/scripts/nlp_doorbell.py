617943ece803eca96f24df81011b6b6b0140917f
Jan Witt <wittjan03@gmail.com>
2025-12-18 22:41:10 +0100

    refactor: Doorbell detection is now refactored to ROS2 as a Node.

--- activate_language_processing/scripts/nlp_doorbell.py
+++ activate_language_processing/scripts/nlp_doorbell.py
@@ -1,64 +1,103 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+
 import numpy as np
-import rospy
-from audio_common_msgs.msg import AudioData
-import scipy.io.wavfile
+import rclpy
 import scipy as sp
-import time
+import scipy.io.wavfile
+from rclpy.node import Node
+from std_msgs.msg import String, UInt8MultiArray
 
-# Every time new data arrives form our microphone execute this function
-def callback_fft(context, data):
+AudioMsg = UInt8MultiArray
+# from audio_common_msgs.msg import AudioData
 
-    # Convert data into int16 np array
-    new_data = np.frombuffer(bytes(data.data), dtype=np.int16)
-    # Remove old samples and append newest
-    context["data"] = np.concatenate([context["data"][len(new_data):], new_data])
-    
-    context["divider"] += 1
-    # Compare only every 100 samples to save computation time 
-    if 100 <= context["divider"]:
-        context["divider"] = 0
-        compare(acc_data["data"], context["threshold"], context["pub"])
 
-# Compare the audio with the reference sound from the .wav file using FFT
-# Convolve audio sample with the reference
-# If we found a peak that is bigger than the threshold we found most likely our bell
-def compare(mic_data, threshold, pub):
-    # s = time.perf_counter() # for performance measurement
-    comp = sp.signal.fftconvolve(mic_data, reference, mode="valid")
+class DoorbellDetectionNode(Node):
+    """
+     A node that detects a doorbell ring to start the challenges.
+     When a doorbell is detected, we publish a string via topic.
+    """
 
-    calc = np.multiply(comp.max(), np.float64(1e-9))
-    # print(time.perf_counter()-s) # for performance measurement
+    def __init__(self, args):
+        # Publisher
+        super().__init__("doorbell_detection")
+        self.publisher = self.create_publisher(String, args.outputTopic, 1)
 
-    if calc > threshold:
-        pub.publish("Doorbell detected!")
+        # Subscriber for microphone data (/audio/audio is HSR?)
+        self.subscription = self.create_subscription(
+            AudioMsg, "/audio/audio", self.callback_fft, 10
+        )
+        self.get_logger().info("Doorbell detection started")
 
-if __name__ == '__main__':
-    rospy.init_node('doorbell_detection', anonymous=True)
-    rospy.loginfo("[ALP]: Doorbell detection node initialized")
+        # Read the reference audio
+        _, self.reference = scipy.io.wavfile.read(args.reference)
+        # Create a new buffer with the length of the reference data
+        self.buffer = np.zeros(len(self.reference), dtype=np.int16)
+        self.divider = 0
 
-    parser = ArgumentParser(prog='nlp_doorbell', description='Publishes a string message to a particular notification topic when a doorbell has rung.')
-    parser.add_argument('-o', '--outputTopic', default="bell_rang", help='Name of topic to send notifications to. Default: bell_rang')
-    parser.add_argument('-r', '--referenceWAV', default="db2_16K.wav", help='File path (recommended: absolute, not relative path) to a WAV file storing the reference doorbell. This WAV MUST be mono (not stereo!), 16ksps, 16bit/sample.')
-    parser.add_argument('-t', '--threshold', default='10.0', help='Threshold to declare the input sound similar enough to the reference doorbell. Default: 10.0')
-    args, unknown = parser.parse_known_args(rospy.myargv()[1:])
+        self.threshold = float(args.threshold)
 
-    # The reference audio file
-    referenceWAVFile = str(args.referenceWAV)
-    threshold = float(args.threshold)
-    # Topic to publish
-    topic = args.outputTopic
+    def callback_fft(self, data: AudioMsg) -> None:
+        """
+        Every time new data arrives form our microphone this function will be executed.
+        It reads the new microphone data and concatenates it to the buffer.
+        Every 100th concatenation we compare the data with the reference.
 
-    # Publish String messages to topic
-    pub = rospy.Publisher(topic, String, queue_size=1)
+        Arguments:
+            data: New audio data from the microphone.
+        """
+        new_data = np.frombuffer(bytes(data.data), dtype=np.int16)
+        self.buffer = np.concatenate([self.buffer[len(new_data) :], new_data])
+        self.get_logger().debug(f"New data received: {self.buffer}")
 
-    _, reference = scipy.io.wavfile.read(referenceWAVFile)
-    context = {"data": np.zeros(len(reference), dtype=np.int16), "reference": reference, "threshold": threshold, "divider": 0, "pub": pub}
+        self.divider += 1
+        if self.divider >= 100:
+            self.divider = 0
+            self.compare()
 
-    # Subscriber for microphone data
-    rospy.Subscriber("/audio/audio", AudioData, lambda data: callback_fft(context, data))
-    rospy.loginfo("[ALP]: Doorbell detection node started")
+    def compare(self) -> None:
+        """
+        Compare data with reference audio using fft.
+        """
+        comp = sp.signal.fftconvolve(self.buffer, self.reference, mode="valid")
+        calc = np.multiply(comp.max(), np.float64(1e-9))
 
-    rospy.spin()
+        if calc > self.threshold:
+            msg = String()
+            msg.data = "Doorbell detected!"
+            self.publisher.publish(msg)
+            self.get_logger().info("Doorbell detected!")
+
+
+def main():
+    rclpy.init()
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "--reference",
+        default="db2_16K.wav",
+        help="File path (recommended: absolute, not relative path) to a WAV file storing the reference doorbell. "
+        "This WAV MUST be mono (not stereo!), 16ksps, 16bit/sample.",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        default=10,
+        help="Threshold to declare the input sound similar enough to the reference doorbell. Default: 10.0",
+    )
+    parser.add_argument(
+        "-o",
+        "--outputTopic",
+        default="bell_rang",
+        help="Name of topic to send notifications to. Default: bell_rang",
+    )
+    parsed_args = parser.parse_args()
+
+    node = DoorbellDetectionNode(parsed_args)
+    rclpy.spin(node)
+
+
+if __name__ == "__main__":
+    main()
