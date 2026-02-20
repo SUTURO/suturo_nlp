9bce02a421abb88c572112414e398fba21993b65
Jan Witt <wittjan03@gmail.com>
2026-02-20 13:41:07 +0100

    nlp refactor

--- activate_language_processing/activate_language_processing/nlp.py
+++ activate_language_processing/activate_language_processing/nlp.py
@@ -17,26 +17,25 @@ def install_spacy_required_packages():
 
 install_spacy_required_packages()
 
-placeholderWords = {"her", "him", "it", "them", "there"}
-conjDeps = {"conj", "dep"}
-auxDeps = {"ccomp"}
-attrDeps = {"acl", "amod", "relcl"}
-numDeps = {"nummod"}
-actAttrPOS = {"VERB"}
-propAttrPOS = {"ADJ", "ADV"}
-numAttrPOS = {"NUM"}
-roleForbiddenDeps = attrDeps.union(numDeps)
+placeholder_words = {"her", "him", "it", "them", "there"}
+conj_deps = {"conj", "dep"}
+aux_deps = {"ccomp"}
+attr_deps = {"acl", "amod", "relcl"}
+num_deps = {"nummod"}
+act_attr_pos = {"VERB"}
+prop_attr_pos = {"ADJ", "ADV"}
+num_attr_pos = {"NUM"}
+role_forbidden_deps = attr_deps.union(num_deps)
 
 
-def inRange(idx, idxS, idxE):
-    return (idxS <= idx) and (idx < idxE)
+def in_range(idx, start_index, end_index):
+    return (start_index <= idx) and (idx < end_index)
 
 
-def rainDance(text):
+def rain_dance(text):
     """
     Perform rain dances to hopefully appease the Neural Network gods
     so as to get a good parse out of a text.
-
     In boring speak, a few preprocessing steps that MIGHT steer spacy
     away from some dumb failures.
     """
@@ -49,78 +48,77 @@ def rainDance(text):
     return text
 
 
-def getAttributes(idxS, idxE, idx2Tok, deps, poss):
-
+def get_attributes(start_index, end_index, index_to_token, dependency, poss):
     attributes = []
-
-    for idx, tok in idx2Tok.items():
-        if not inRange(idx, idxS, idxE):
+    for idx, token in index_to_token.items():
+        if not in_range(idx, start_index, end_index):
             continue
 
-        for c in tok.children:
-            if (
-                (not inRange(c.idx, idxS, idxE))
-                and (c.dep_ in deps)
-                and (c.pos_ in poss)
-            ):
-                _, text, _ = getSubtree(c)
-                attributes.append(text)
+        for child in token.children:
+            if in_range(child.idx, start_index, end_index):
+                continue
+            if child.dep_ not in dependency:
+                continue
+            if child.pos_ not in poss:
+                continue
+
+            _, text, _ = get_subtree(child)
+            attributes.append(text)
 
     return tuple(attributes)
 
 
-def subtreeDep(idxS, idxE, idx2Tok):
-    inSpan = set()
-    idx2Dep = {}
+def subtree_dep(start_index, end_index, index_to_token):
+    in_span = set()
+    idx_to_dep = {}
 
-    for idx, tok in idx2Tok.items():
-        if inRange(idx, idxS, idxE):
-            inSpan.add(idx)
-            idx2Dep[idx] = (tok.head.idx, tok.dep_)
+    for idx, token in index_to_token.items():
+        if in_range(idx, start_index, end_index):
+            in_span.add(idx)
+            idx_to_dep[idx] = (token.head.idx, token.dep_)
 
-    for idx, (hIdx, dep) in idx2Dep.items():
-        if hIdx not in inSpan:
+    for idx, (hIdx, dep) in idx_to_dep.items():
+        if hIdx not in in_span:
             return dep
 
     return None
 
 
-def parseIntent(cspec, context):
+def parse_intent(cspec, context):
     """
     Use RASA to parse a simple sentence (one intent).
     """
     text = cspec["text"]
-    sStart = cspec["start"]
-    idx2Tok = cspec["idx2Tok"]
-
+    sentence_start = cspec["start"]
+    index_to_token = cspec["index_to_token"]
     req = {"text": text}
     r = requests.post(context["rasaURI"], data=bytes(json.dumps(req), "utf-8"))
     response = json.loads(r.text)
 
     result = {"sentence": text, "intent": response["intent"]["name"], "entities": {}}
 
-    for k, e in enumerate(response["entities"]):
-        # print("Entity", e, sStart)
-        eStart = e.get("start", 0) + sStart
-        eEnd = e.get("end", 0) + sStart
+    for k, entity in enumerate(response["entities"]):
+        # print("Entity", entity, sentence_start)
+        entity_start = entity.get("start", 0) + sentence_start
+        entity_end = entity.get("end", 0) + sentence_start
 
-        if subtreeDep(eStart, eEnd, idx2Tok) in roleForbiddenDeps:
+        if subtree_dep(entity_start, entity_end, index_to_token) in role_forbidden_deps:
             continue
 
         result["entities"][k] = {
             "idx": k,
-            "role": e.get("role", "UndefinedRole"),
-            "value": e.get("value", "UnparsedEntity"),
-            "group": int(e.get("group", 0)),
-            "entity": e.get("entity", "owl:Thing"),
-            "propertyAttribute": getAttributes(
-                eStart, eEnd, idx2Tok, attrDeps, propAttrPOS
+            "role": entity.get("role", "UndefinedRole"),
+            "value": entity.get("value", "UnparsedEntity"),
+            "group": int(entity.get("group", 0)),
+            "entity": entity.get("entity", "owl:Thing"),
+            "propertyAttribute": get_attributes(
+                entity_start, entity_end, index_to_token, attr_deps, prop_attr_pos
             ),
-            "actionAttribute": getAttributes(
-                eStart, eEnd, idx2Tok, attrDeps, actAttrPOS
+            "actionAttribute": get_attributes(
+                entity_start, entity_end, index_to_token, attr_deps, act_attr_pos
             ),
-            "numberAttribute": getAttributes(
-                eStart, eEnd, idx2Tok, numDeps, numAttrPOS
+            "numberAttribute": get_attributes(
+                entity_start, entity_end, index_to_token, num_deps, num_attr_pos
             ),
         }
 
@@ -133,34 +131,30 @@ def degroup(parses):
     of parses.
     """
     print(parses)
+
     results = []
 
     for e in parses:
         intent = e["intent"]
         entities = e["entities"]
-
         groups = {0: {}}
 
         for k, ed in entities.items():
             role = ed["role"]
             group = ed["group"]
-
             if group not in groups:
                 groups[group] = {}
             if role not in groups[group]:
                 groups[group][role] = []
-
             groups[group][role].append(ed)
 
         for k in sorted(groups.keys()):
             eds = {}
-
             for r, vs in groups[k].items():
                 for v in vs:
                     v_copy = v.copy()
                     v_copy["group"] = 0
                     eds[v["idx"]] = v_copy
-
                     # eds[v["idx"]] = v.copy()
                     # eds[v["idx"]]["group"] = 0
 
@@ -171,58 +165,59 @@ def degroup(parses):
     return results
 
 
-def getSubtree(tok):
+def get_subtree(token):
     """
     Return the subtree of a token, but stop at dependent verbs.
     This allows splitting a text into sentences.
     """
-    inText = [(tok.idx, tok)]
-    todo = list(tok.children)
-    next = []
-    idx2Tok = {tok.idx: tok}
+    in_text = [(token.idx, token)]
+    todo = list(token.children)
+    next_tokens = []
+    index_to_token = {token.idx: token}
     excluded = set()
 
-    for c in tok.children:
-        # if (("VERB" == c.pos_) and (c.dep_ in conjDeps)) or (("AUX" == c.pos_ ) and ((c.dep_ in conjDeps) or (c.dep_ in auxDeps)) and ("be" == c.lemma_)):
-        if ("VERB" == c.pos_) and (c.dep_ in conjDeps):
-            next.append(c)
-            excluded.add(c.idx)
+    for child in token.children:
+        # if (("VERB" == child.pos_) and (child.dep_ in conj_deps)) or (("AUX" == child.pos_ ) and ((child.dep_ in conj_deps) or (child.dep_ in aux_deps)) and ("be" == child.lemma_)):
+        if ("VERB" == child.pos_) and (child.dep_ in conj_deps):
+            next_tokens.append(child)
+            excluded.add(child.idx)
 
     while todo:
-        cr = todo.pop()
+        current_token = todo.pop()
 
-        if cr.idx not in excluded:
-            inText.append((cr.idx, cr))
-            idx2Tok[cr.idx] = cr
-            todo.extend(list(cr.children))
+        if current_token.idx not in excluded:
+            in_text.append((current_token.idx, current_token))
+            index_to_token[current_token.idx] = current_token
+            todo.extend(list(current_token.children))
 
-    toks = sorted(inText, key=lambda x: x[0])
+    toks = sorted(in_text, key=lambda x: x[0])
     subtree_t = " ".join([str(token) for idx, token in toks])
 
-    return next, subtree_t, idx2Tok
+    return next_tokens, subtree_t, index_to_token
 
 
-def splitIntents(text, context):
+def split_intents(text, context):
     doc = context["nlp"](text)
-    intentUtterances = []
+    intent_utterances = []
 
     for s in doc.sents:
         todo = [s.root]
-
         while todo:
-            cr = todo.pop()
-            next, text, idx2Tok = getSubtree(cr)
-            todo.extend(next)
-
-            intentUtterances.append(
-                {"text": text, "start": min(idx2Tok.keys()), "idx2Tok": idx2Tok}
+            current_token = todo.pop()
+            next_tokens, text, index_to_token = get_subtree(current_token)
+            todo.extend(next_tokens)
+            intent_utterances.append(
+                {
+                    "text": text,
+                    "start": min(index_to_token.keys()),
+                    "index_to_token": index_to_token,
+                }
             )
 
-    return intentUtterances
+    return intent_utterances
 
 
-def guessRoles(parses, context, needsGuessFn):
-
+def guess_roles(parses, context, needs_guess_fn):
     def _te2de(entities):
         results = {}
         for k, v in entities.items():
@@ -244,58 +239,56 @@ def guessRoles(parses, context, needsGuessFn):
 
         return results
 
-    roleMap = {}
+    role_map = {}
     results = []
 
-    for e in parses:
-        intent = e["intent"]
-        entities = _te2de(e["entities"])
+    for parse in parses:
+        intent = parse["intent"]
+        entities = _te2de(parse["entities"])
 
         for role, fillers in entities.items():
-            if needsGuessFn(set([x["value"] for x in fillers])):
+            if needs_guess_fn(set([x["value"] for x in fillers])):
                 for guessedRole in {role}.union(context["role2Roles"].get(role, [])):
-                    if guessedRole in roleMap:
-                        entities[role] = roleMap[guessedRole]
+                    if guessedRole in role_map:
+                        entities[role] = role_map[guessedRole]
                         break
             elif len(fillers) > 0:
-                roleMap[role] = fillers
+                role_map[role] = fillers
 
         results.append(
-            {"sentence": e["sentence"], "intent": intent, "entities": _de2te(entities)}
+            {
+                "sentence": parse["sentence"],
+                "intent": intent,
+                "entities": _de2te(entities),
+            }
         )
 
     return results
 
 
-def semanticLabelling(text, context):
+def semantic_labelling(text, context):
     text = text.strip()
-    text = rainDance(text)
-
-    intentUtterances = splitIntents(text, context)
-    parsedIntents = degroup([parseIntent(x, context) for x in intentUtterances])
-    parsedIntents = guessRoles(
-        parsedIntents, context, lambda x: 0 != len(x.intersection(placeholderWords))
+    text = rain_dance(text)
+    intent_utterances = split_intents(text, context)
+    parsed_intents = degroup([parse_intent(x, context) for x in intent_utterances])
+    parsed_intents = guess_roles(
+        parsed_intents, context, lambda x: 0 != len(x.intersection(placeholder_words))
     )
 
-    for k, e in enumerate(parsedIntents):
+    for k, e in enumerate(parsed_intents):
         no_entities = len(e["entities"]) == 0
-        not_last = k < len(parsedIntents) - 1
+        not_last = k < len(parsed_intents) - 1
 
         if no_entities and not_last:
-            next_ent = parsedIntents[k + 1]["entities"]
+            next_entities = parsed_intents[k + 1]["entities"]
             intent_roles = context["intent2roles"].get(e["intent"], {})
-
             j = 0
-            for espec in next_ent.values():
-                role = espec.get("role", "UndefinedRole")
 
+            for entity_spec in next_entities.values():
+                role = entity_spec.get("role", "UndefinedRole")
                 if role in intent_roles:
-                    ent_copy = espec.copy()
-                    ent_copy["entities"][j] = ent_copy
-                    ent_copy["idx"] = j
+                    e["entities"][j] = entity_spec.copy()
+                    e["entities"][j]["idx"] = j
                     j += 1
-                    # e["entities"][j] = espec.copy()
-                    # e["entities"][j]["idx"] = j
-                    # j += 1
 
-    return parsedIntents
+    return parsed_intents
